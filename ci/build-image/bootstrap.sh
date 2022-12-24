e3520aade4300261df681aacbd2a983d68850fa3
James Rayner <james@ecreationmedia.tv>
2022-12-24 11:43:21 +0000

    First commit of CI stuff

---
+++ ci/build-image/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+echo "Updating distro..."
+apt-get update
+
+echo "==================="
+echo "= Install BeebAsm ="
+echo "==================="
+
+echo "Cloning repo..."
+git clone https://github.com/stardot/beebasm/
+
+echo "Building..."
+cd beebasm/src
+make code
+
+echo "Moving binary to /usr/local/bin..."
+ls -l ../beebasm
+cp ../beebasm /usr/local/bin
+
+echo "Fininshed."
